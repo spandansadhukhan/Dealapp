7dcad21eb408da90bc47239000e10da45ad98363
bikash_mondal <nits.bikash@gmail.com>
2018-03-14 20:01:26 +0530

    lastupdate

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams,LoadingController } from 'ionic-angular';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 /**
  * Generated class for the HomePage page.
  *
@@ -14,12 +14,88 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home.html',
 })
 export class HomePage {
+  public response:any;
+  //public brandresult:any;
+ responseData : any;
+ public popularcategorylist:any;
+ public categorylist:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public authService:AuthServiceProvider,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+    this.popularcategory();
+    this.category();
   }
+
+
+  popularcategory(){
+   
+     
+    this.authService.getData('users/popular_category_list_api').then((result) => {
+      this.responseData = result
+  console.log("CHATRESULTTTT",result);
+      if( this.responseData.ACK == 1)
+      {
+       
+        this.popularcategorylist =  this.responseData.Category;
+        //this.popularcategoryname=this.responseData.model.name;
+        //this.popularcategoryimage=this.responseData.model.image;
+        
+        //console.log('rrrrrrrrrrrrrrrrrrrrrreview',this.popularcategorylist);
+      }
+      else
+      {
+        this.popularcategorylist = '';
+      }
+     
+    }, (err) => {
+      console.log(err);
+      // Error log
+    });
+  }
+
+
+  category(){
+   
+     
+    this.authService.getData('users/category_list_api').then((result) => {
+      this.responseData = result
+  console.log("CHATRESULTTTT",result);
+      if( this.responseData.ACK == 1)
+      {
+       
+        this.categorylist =  this.responseData.Category;
+        
+        
+        console.log('rrrrrrrrrrrrrrrrrrrrrreview',this.categorylist);
+      }
+      else
+      {
+        this.categorylist = '';
+      }
+     
+    }, (err) => {
+      console.log(err);
+      // Error log
+    });
+  }
+
+  deallist(type,id){
+    //alert(type);
+    this.navCtrl.push('DeallistPage',{'type':type,'id':id});
+    //localStorage.setItem('modeldata', JSON.stringify(name));
+  }
+
+  couponlist(type,id){
+    //alert(type);
+    this.navCtrl.push('CouponlistPage',{'type':type,'id':id});
+    //localStorage.setItem('modeldata', JSON.stringify(name));
+  }
+
 
 }
