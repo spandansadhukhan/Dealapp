744d44cd2dec146c88c8dc15543906c272fa7f8a
bikash_mondal <nits.bikash@gmail.com>
2018-03-15 16:55:07 +0530

    eve update

--- src/pages/dealadd/dealadd.ts
+++ src/pages/dealadd/dealadd.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 /**
  * Generated class for the DealaddPage page.
  *
@@ -15,11 +16,53 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DealaddPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public response:any;
+   responseData : any;
+ public shoplist:any;
+ public id:any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private storage: Storage,
+    public authService:AuthServiceProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DealaddPage');
+    //this.id = this.storage.get('uid');
+   //alert(this.id);
+    //this.usershop(this.id);
   }
+
+
+  usershop(id){
+    this.storage.get('uid').then(val => {
+    console.log(val);
+    });
+    //alert(type);
+    let serval={
+      "id": id
+     };
+     
+    this.authService.postData(serval,'products/usershop_list_api').then((result) => {
+      this.responseData = result
+ // console.log("CHATRESULTTTT",result);
+      if( this.responseData.ACK == 1)
+      {
+       
+        this.shoplist =  this.responseData.Shop; 
+        
+        console.log('ccc',this.shoplist);
+      }
+      else
+      {
+        this.shoplist = '';
+        
+      }
+     
+    }, (err) => {
+      console.log(err);
+      // Error log
+    });
+  }
+
 
 }
