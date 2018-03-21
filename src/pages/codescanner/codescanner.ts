e7fb04ff52a2e9ab48869d278449d37f087c99bb
Surbhi Seth <surbhi@natitsolved.com>
2018-03-21 10:15:48 +0530

    Barcode scanner & Paypal

--- src/pages/codescanner/codescanner.ts
+++ src/pages/codescanner/codescanner.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angu
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { CodescannerProvider } from "../../providers/codescanner/codescanner";
 import { Storage } from '@ionic/storage';
+// import { PayPal } from "@ionic-native/paypal";
+import { PayPal, PayPalPayment, PayPalConfiguration } from '@ionic-native/paypal';
 /**
  * Generated class for the CodescannerPage page.
  *
@@ -22,8 +24,9 @@ export class CodescannerPage {
     private barcodeScanner: BarcodeScanner,
     public codeService: CodescannerProvider,
     public storage: Storage,
-    public alertCtrl: AlertController) {
-    this.startScanner();
+    public alertCtrl: AlertController,
+    private payPal: PayPal) {
+    //this.startScanner();
   }
 
   startScanner()
@@ -32,6 +35,8 @@ export class CodescannerPage {
       this.barcodeScanner.scan().then(barcodeData => {
         console.log('Barcode data', barcodeData);
         var scanData: any = { user_id: val, loyalty_code : barcodeData.text}
+        if (barcodeData.text)
+        {
         this.codeService.findCode(scanData).subscribe(resData => 
           {
           if (resData.ACK == 1) {
@@ -50,13 +55,56 @@ export class CodescannerPage {
             alert.present();
             
           }
-          this.startScanner();
+          //this.startScanner();
           })
+        }
         //alert(barcodeData.text)
       }).catch(err => {
         console.log('Error', err);
       });
     })
+  }
+
+  paypalPayment(){
+    this.payPal.init({
+      PayPalEnvironmentProduction: 'YOUR_PRODUCTION_CLIENT_ID',
+      PayPalEnvironmentSandbox: 'AS5crcxYYgjiPybED_DVEXA1I18IkTpkuU_N_16BaEJyNcHveb7830REmz2fwPeHc9_uZqQwdOeIzTHX'
+    }).then(() => {
+      // Environments: PayPalEnvironmentNoNetwork, PayPalEnvironmentSandbox, PayPalEnvironmentProduction
+      this.payPal.prepareToRender('PayPalEnvironmentSandbox', new PayPalConfiguration({
+        // Only needed if you get an "Internal Service Error" after PayPal login!
+        //payPalShippingAddressOption: 2 // PayPalShippingAddressOptionPayPal
+      })).then(() => {
+        let payment = new PayPalPayment('3.33', 'USD', 'Description', 'sale');
+        this.payPal.renderSinglePaymentUI(payment).then(() => {
+          // Successfully paid
+
+          // Example sandbox response
+          //
+          // {
+          //   "client": {
+          //     "environment": "sandbox",
+          //     "product_name": "PayPal iOS SDK",
+          //     "paypal_sdk_version": "2.16.0",
+          //     "platform": "iOS"
+          //   },
+          //   "response_type": "payment",
+          //   "response": {
+          //     "id": "PAY-1AB23456CD789012EF34GHIJ",
+          //     "state": "approved",
+          //     "create_time": "2016-10-03T13:33:33Z",
+          //     "intent": "sale"
+          //   }
+          // }
+        }, () => {
+          // Error or render dialog closed without being successful
+        });
+      }, () => {
+        // Error in configuration
+      });
+    }, () => {
+      // Error in initialization, maybe PayPal isn't supported or something else
+    });
   }
 
   ionViewDidLoad() {
