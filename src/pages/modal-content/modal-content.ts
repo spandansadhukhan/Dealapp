73e4d88dee9854b95d92d6c9320f2099e563e35c
bikash_mondal <nits.bikash@gmail.com>
2018-03-30 14:25:25 +0530

    noon update

--- src/pages/modal-content/modal-content.ts
+++ src/pages/modal-content/modal-content.ts
@@ -14,7 +14,7 @@ import { IonicPage, NavController, NavParams,Platform,ViewController } from 'ion
 <ion-header>
   <ion-toolbar>
     <ion-title>
-      Description
+      Click below to get your coupon code
     </ion-title>
     <ion-buttons start>
       <button ion-button (click)="dismiss()">
@@ -25,21 +25,19 @@ import { IonicPage, NavController, NavParams,Platform,ViewController } from 'ion
   </ion-toolbar>
 </ion-header>
 <ion-content>
-  <ion-list>
-      <ion-item>
-        <ion-avatar item-start>
-          <img src="">
-        </ion-avatar>
-        <h2>Spandan</h2>
-        <p>web developer</p>
-      </ion-item>
-      <ion-item >
-        development
-        <ion-note item-end>
-          good
-        </ion-note>
-      </ion-item>
-  </ion-list>
+  <ion-grid class="coupon-dtl">
+    <ion-row>
+        <ion-col col-2></ion-col>
+        <ion-col col-8>
+        <button ion-button block color="secondary" *ngIf="c_type == 'S'" (click)="purchase(c_id,c_type)">Redeem Coupon Code</button>
+        <button ion-button block color="secondary" *ngIf="c_type == 'O'" (click)="purchase(c_id,c_type)">Purchase Coupon Code</button>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col col-12><h6 class="text-center">Amazon: Up To 75% Off | Amazon Promo Codes & Coupons February 2018</h6></ion-col>
+        <ion-col col-12><p class="text-center">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p></ion-col>
+      </ion-row>
+  </ion-grid>
 </ion-content>
 `
 })
@@ -47,9 +45,11 @@ import { IonicPage, NavController, NavParams,Platform,ViewController } from 'ion
 export class ModalContentPage {
   character;
 
+  public c_type: any;
+  public c_id: any;
   constructor(
     public platform: Platform,
-    public params: NavParams,
+    public navParams: NavParams,
     public viewCtrl: ViewController,
     public navCtrl: NavController
   ) {
@@ -62,6 +62,20 @@ export class ModalContentPage {
    this.navCtrl.push('CouponlistPage');
   }
 
+  purchase(coupon_id, coupon_type) {
 
+    this.navCtrl.push('CouponpurchasePage', { 'coupon_id': coupon_id, 'coupon_type': coupon_type });
+
+  }
   
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ModalContentPage');
+    this.c_type = this.navParams.get('coupon_type');
+    this.c_id = this.navParams.get('coupon_id');
+  }
+
+
+
+
 }
